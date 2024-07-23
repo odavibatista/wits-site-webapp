32b89ec9f620a6c834670a5f00444cc6491af12b
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2024-07-23 14:12:58 -0300

    Correção no menu mobile, adição da mock platform e inclusão das plataformas na terceira seção

--- src/components/button/BtnPlatform.tsx
+++ src/components/button/BtnPlatform.tsx
@@ -5,13 +5,13 @@ interface Props {
 
 export function BtnPlatform({ platformName, imageUrl }: Props) {
   return (
-    <div className="m-2 flex h-9 w-44 items-center justify-center gap-4 rounded-lg text-white">
+    <div className="m-2 flex h-9 w-44 items-center justify-center gap-4 rounded-lg text-white cursor-pointer transition duration-300 hover:scale-110">
       <img
         className="h-8 w-8"
         src={imageUrl}
         alt={`Logo da plataforma ${platformName}`}
       />
-      <p className="font-mono text-lg">{platformName}</p>
+      <p className="font-mono text-sm md:text-base lg:text-lg">{platformName}</p>
     </div>
   )
 }
