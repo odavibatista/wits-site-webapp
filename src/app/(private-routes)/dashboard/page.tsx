77c1b1ff7f849485818fba7935dff78f0d83754e
Davi Batista <davideosmar11@gmail.com>
2024-07-26 21:47:32 -0300

    feat: created basic quiz screen

--- src/app/(private-routes)/dashboard/page.tsx
+++ src/app/(private-routes)/dashboard/page.tsx
@@ -4,9 +4,6 @@ import HeaderAuth from '@/presentation/components/header-auth'
 import { useHomeData } from '../provider-home-data'
 import DashboardUser from '@/presentation/components/dashboard'
 import { Footer } from '@/presentation/components/landing-page/Footer'
-import { useEffect, useState } from 'react'
-import { browseCourses, IBrowseCoursesResponse } from '../../api/course/browse-courses.endpoint'
-import Option from '../../../presentation/components/course/Option'
 
 export default function Dashboard() {
 
@@ -14,7 +11,6 @@ export default function Dashboard() {
     <div className="text-neutral-50">
       <HeaderAuth />
       <DashboardUser />
-      <Option index={'A'} id_activity={1} option='A' />
       <Footer />
     </div>
   )
